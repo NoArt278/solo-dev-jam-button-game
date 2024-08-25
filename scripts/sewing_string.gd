c9519ee11b9e8d968364bf607d37ff8a3129dfc2
Nat10k <156871636+NoArt278@users.noreply.github.com>
2024-08-25 12:51:31 +0700

    fix : mouse cursor collision with string

--- scripts/sewing_string.gd
+++ scripts/sewing_string.gd
@@ -4,7 +4,7 @@ var is_mouse_hovered : bool = false
 @onready var string_joint: DampedSpringJoint2D = $StringJoint
 @onready var collision_shape_2d: CollisionShape2D = $Area2D/CollisionShape2D
 static var instantiated_line : Line2D
-var col_shape : SegmentShape2D
+var col_shape : RectangleShape2D
 var connected_button : Node2D
 var parent_button : Node2D
 
@@ -17,13 +17,13 @@ func _process(_delta: float) -> void:
 	if (Input.is_action_just_pressed("destroy_string") and is_mouse_hovered) or (Input.is_action_just_released("add_string") and (string_joint.node_a == null or string_joint.node_b.is_empty())):
 		instantiated_line = null
 		queue_free()
-		print("click destroyed or not connected")
 	if Input.is_action_pressed("add_string") and string_joint.node_b.is_empty():
 		set_point_position(1, to_local(get_global_mouse_position()))
-		col_shape.b = get_point_position(1)
 	if (!string_joint.node_b.is_empty()) :
 		set_point_position(1, to_local(connected_button.position))
-		col_shape.b = get_point_position(1)
+	collision_shape_2d.position = get_point_position(1)/2
+	collision_shape_2d.rotation = global_position.angle_to(to_global(get_point_position(1)))
+	col_shape.size.x = position.distance_to(get_point_position(1))
 
 func _on_area_2d_mouse_entered() -> void:
 	print("mouse entered")
@@ -34,7 +34,9 @@ func _on_area_2d_mouse_exited() -> void:
 	is_mouse_hovered = false
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("body entered")
 	if body is ClotheButton and parent_button != body and connected_button == null:
+		print("button entered")
 		string_joint.node_b = body.get_path()
 		string_joint.length = parent_button.global_position.distance_to(body.global_position)
 		connected_button = body
