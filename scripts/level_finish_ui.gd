172d50acd03ff0e710f8475997a60c16a5f04bdc
Nat10k <156871636+NoArt278@users.noreply.github.com>
2024-08-27 00:00:39 +0700

    feat : ending scene and make lvl 5 a bit more forgiving

--- scripts/level_finish_ui.gd
+++ scripts/level_finish_ui.gd
@@ -1,10 +1,14 @@
 extends Control
 
 static var curr_level = 1
+const max_level = 5
 
 func _on_retry_button_pressed() -> void:
 	get_tree().reload_current_scene()
 
 func _on_next_level_button_pressed() -> void:
 	curr_level += 1
-	get_tree().change_scene_to_file("res://scenes/levels/level_" + str(curr_level) + ".tscn")
+	if curr_level <= max_level :
+		get_tree().change_scene_to_file("res://scenes/levels/level_" + str(curr_level) + ".tscn")
+	else :
+		get_tree().change_scene_to_file("res://scenes/end.tscn")
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,3 +2,6 @@ extends Control
 
 func start_game():
 	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
+
+func back_to_main():
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
